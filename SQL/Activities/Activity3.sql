f6baba7bec366e824fd15b0718d8c00c376be20a
KusumKumari001 <154735769+KusumKumari001@users.noreply.github.com>
2023-12-25 09:41:03 +0530

    Add files via upload

---
+++ SQL/Activities/Activity3.sql
@@ -0,0 +1,6 @@
+select SALESMAN_ID,SALESMAN_CITY from salesman;
+
+select * from salesman where SALESMAN_CITY='Paris';
+
+select SALESMAN_ID,COMMISSION from salesman where SALESMAN_NAME='Paul Adam';
+
---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,11 @@
+REM   Script: Activity4
+REM   Activity4
+
+Alter table salesman add grade int;
+
+SELECT * FROM salesman;
+
+Update salesman set grade=100;
+
+SELECT * FROM salesman;
+
